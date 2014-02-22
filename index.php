5e1e4601e29b5f41892dc1c65e99fe94b28ee8e4
Aubrey Portwood <aubreypwd@gmail.com>
2014-02-22 12:07:03 -0700

    Removing SVN and HG history

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 
 /*
-Plugin Name: Remove Protected (Development Mode)
+Plugin Name: Remove Protected
 Description: This small plugin will remove "Protected: " from your title names on pages that are password protected.
 Author: Aubrey Portwood
 Author URI: http://aubreypwd.com/
+Version: 1.0
 */
 
-include "trunk/index.php";
+add_filter( 'private_title_format', 'aubreypwd_remove_protected' );
+add_filter( 'protected_title_format', 'aubreypwd_remove_protected' );
+ 
+function aubreypwd_remove_protected( $format ) {
+    return '%s';
+}
 
 ?>
