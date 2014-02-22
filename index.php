4b3192478584e412df81b44d5bdd7efdb53fbc05
Aubrey Portwood <aubreypwd@gmail.com>
2014-02-22 12:05:16 -0700

    Adding SVN and HG history

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Remove Protected (Development Mode)
+Description: This small plugin will remove "Protected: " from your title names on pages that are password protected.
+Author: Aubrey Portwood
+Author URI: http://aubreypwd.com/
+*/
+
+include "trunk/index.php";
+
+?>
